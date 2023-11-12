3443ceb6a0bc42e62f8c0f0e19517b82e3f6773b
nasperus <gio.melikidze095@gmail.com>
2023-11-12 15:58:45 +0400

    update

--- Assets/Scripts/PlayerMoveState.cs
+++ Assets/Scripts/PlayerMoveState.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMoveState : PlayerState
+public class PlayerMoveState : PlayerGroundedState
 {
     public PlayerMoveState(Player player, PlayerStateMachine stateMachine, string animBoolName) : base(player, stateMachine, animBoolName)
     {
